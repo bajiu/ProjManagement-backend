45f00860f9d904193c956ceadff1b0e0b2f987c9
bajiu <bajiu0819@163.com>
2024-01-30 19:26:18 +0800

    change nemu

--- data/menu.js
+++ data/menu.js
@@ -36,35 +36,35 @@ const menu = [
                 "path": "/dashboard/dataVisualize",
                 "title": "数据可视化"
             },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/dashboard/embedded",
-                "title": "内嵌页面"
-            }
+            // {
+            //     "icon": "AppstoreOutlined",
+            //     "path": "/dashboard/embedded",
+            //     "title": "内嵌页面"
+            // }
         ]
     },
-    {
-        "icon": "FileTextOutlined",
-        "title": "表单 Form",
-        "path": "/form",
-        "children": [
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/form/basicForm",
-                "title": "基础 Form"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/form/validateForm",
-                "title": "校验 Form"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/form/dynamicForm",
-                "title": "动态 Form"
-            }
-        ]
-    },
+    // {
+    //     "icon": "FileTextOutlined",
+    //     "title": "表单 Form",
+    //     "path": "/form",
+    //     "children": [
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/form/basicForm",
+    //             "title": "基础 Form"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/form/validateForm",
+    //             "title": "校验 Form"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/form/dynamicForm",
+    //             "title": "动态 Form"
+    //         }
+    //     ]
+    // },
     {
         "icon": "PieChartOutlined",
         "title": "Echarts",
@@ -102,137 +102,137 @@ const menu = [
             }
         ]
     },
-    {
-        "icon": "ShoppingOutlined",
-        "title": "常用组件",
-        "path": "/assembly",
-        "children": [
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/assembly/guide",
-                "title": "引导页"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/assembly/svgIcon",
-                "title": "Svg 图标"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/assembly/selectIcon",
-                "title": "Icon 选择"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/assembly/batchImport",
-                "title": "批量导入数据"
-            }
-        ]
-    },
-    {
-        "icon": "ProfileOutlined",
-        "title": "菜单嵌套",
-        "path": "/menu",
-        "children": [
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/menu/menu1",
-                "title": "菜单1"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/menu/menu2",
-                "title": "菜单2",
-                "children": [
-                    {
-                        "icon": "AppstoreOutlined",
-                        "path": "/menu/menu2/menu21",
-                        "title": "菜单2-1"
-                    },
-                    {
-                        "icon": "AppstoreOutlined",
-                        "path": "/menu/menu2/menu22",
-                        "title": "菜单2-2",
-                        "children": [
-                            {
-                                "icon": "AppstoreOutlined",
-                                "path": "/menu/menu2/menu22/menu221",
-                                "title": "菜单2-2-1"
-                            },
-                            {
-                                "icon": "AppstoreOutlined",
-                                "path": "/menu/menu2/menu22/menu222",
-                                "title": "菜单2-2-2"
-                            }
-                        ]
-                    },
-                    {
-                        "icon": "AppstoreOutlined",
-                        "path": "/menu/menu2/menu23",
-                        "title": "菜单2-3"
-                    }
-                ]
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/menu/menu3",
-                "title": "菜单3"
-            }
-        ]
-    },
-    {
-        "icon": "ExclamationCircleOutlined",
-        "title": "错误页面",
-        "path": "/error",
-        "children": [
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/404",
-                "title": "404页面"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/403",
-                "title": "403页面"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/500",
-                "title": "500页面"
-            }
-        ]
-    },
-    {
-        "icon": "PaperClipOutlined",
-        "title": "外部链接",
-        "path": "/link",
-        "children": [
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/link/gitee",
-                "title": "Gitee 仓库",
-                "isLink": "https://gitee.com/laramie/Hooks-Admin"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/link/github",
-                "title": "GitHub 仓库",
-                "isLink": "https://github.com/HalseySpicy/Hooks-Admin"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/link/juejin",
-                "title": "掘金文档",
-                "isLink": "https://juejin.cn/user/3263814531551816/posts"
-            },
-            {
-                "icon": "AppstoreOutlined",
-                "path": "/link/myBlog",
-                "title": "个人博客",
-                "isLink": "http://www.spicyboy.cn"
-            }
-        ]
-    }
+    // {
+    //     "icon": "ShoppingOutlined",
+    //     "title": "常用组件",
+    //     "path": "/assembly",
+    //     "children": [
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/assembly/guide",
+    //             "title": "引导页"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/assembly/svgIcon",
+    //             "title": "Svg 图标"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/assembly/selectIcon",
+    //             "title": "Icon 选择"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/assembly/batchImport",
+    //             "title": "批量导入数据"
+    //         }
+    //     ]
+    // },
+    // {
+    //     "icon": "ProfileOutlined",
+    //     "title": "菜单嵌套",
+    //     "path": "/menu",
+    //     "children": [
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/menu/menu1",
+    //             "title": "菜单1"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/menu/menu2",
+    //             "title": "菜单2",
+    //             "children": [
+    //                 {
+    //                     "icon": "AppstoreOutlined",
+    //                     "path": "/menu/menu2/menu21",
+    //                     "title": "菜单2-1"
+    //                 },
+    //                 {
+    //                     "icon": "AppstoreOutlined",
+    //                     "path": "/menu/menu2/menu22",
+    //                     "title": "菜单2-2",
+    //                     "children": [
+    //                         {
+    //                             "icon": "AppstoreOutlined",
+    //                             "path": "/menu/menu2/menu22/menu221",
+    //                             "title": "菜单2-2-1"
+    //                         },
+    //                         {
+    //                             "icon": "AppstoreOutlined",
+    //                             "path": "/menu/menu2/menu22/menu222",
+    //                             "title": "菜单2-2-2"
+    //                         }
+    //                     ]
+    //                 },
+    //                 {
+    //                     "icon": "AppstoreOutlined",
+    //                     "path": "/menu/menu2/menu23",
+    //                     "title": "菜单2-3"
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/menu/menu3",
+    //             "title": "菜单3"
+    //         }
+    //     ]
+    // },
+    // {
+    //     "icon": "ExclamationCircleOutlined",
+    //     "title": "错误页面",
+    //     "path": "/error",
+    //     "children": [
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/404",
+    //             "title": "404页面"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/403",
+    //             "title": "403页面"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/500",
+    //             "title": "500页面"
+    //         }
+    //     ]
+    // },
+    // {
+    //     "icon": "PaperClipOutlined",
+    //     "title": "外部链接",
+    //     "path": "/link",
+    //     "children": [
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/link/gitee",
+    //             "title": "Gitee 仓库",
+    //             "isLink": "https://gitee.com/laramie/Hooks-Admin"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/link/github",
+    //             "title": "GitHub 仓库",
+    //             "isLink": "https://github.com/HalseySpicy/Hooks-Admin"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/link/juejin",
+    //             "title": "掘金文档",
+    //             "isLink": "https://juejin.cn/user/3263814531551816/posts"
+    //         },
+    //         {
+    //             "icon": "AppstoreOutlined",
+    //             "path": "/link/myBlog",
+    //             "title": "个人博客",
+    //             "isLink": "http://www.spicyboy.cn"
+    //         }
+    //     ]
+    // }
 ];
 
 module.exports = menu;
